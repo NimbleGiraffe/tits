749366c2ebec77277b3eea0c0f3a1d9ee1ec46b7
Ben Stueck <willbestuck@gmail.com>
2011-09-09 00:02:52 -0400

    ignore file

--- tits/nips/admin.py
+++ tits/nips/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from nips.models import Nipple, NippleOpinion
+from nips.models import Nipple, NippleOpinion, DayFour
 
 class NippleAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'score', 'votes', 'hometown']
@@ -9,5 +9,9 @@ class NippleOpinionAdmin(admin.ModelAdmin):
     list_display = ['user', 'nipple', 'score', 'comment']
     search_fields = ['user__first_name', 'user__last_name', 'nipple__first_name', 'nipple__last_name']
     
+class DayFourAdmin(admin.ModelAdmin):
+    list_display = ['nipple']
+    
 admin.site.register(Nipple, NippleAdmin)
 admin.site.register(NippleOpinion, NippleOpinionAdmin)
+admin.site.register(DayFour, DayFourAdmin)
